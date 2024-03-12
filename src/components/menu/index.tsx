d12393f7a4a3bc5e4eaea4bb254615c6edc0ee5e
Abdullah Numan <anewman15@hotmail.com>
2024-03-12 11:40:59 +0600

    feat: update menu, breadcrumb, layout

--- src/components/menu/index.tsx
+++ src/components/menu/index.tsx
@@ -1,15 +1,20 @@
-import { useMenu } from "@refinedev/core";
+import { useLogout, useMenu } from "@refinedev/core";
 import { NavLink } from "react-router-dom";
+import { Button } from "../ui/button";
 
 export const Menu = () => {
+  const { mutate: logout } = useLogout();
   const { menuItems } = useMenu();
 
   return (
     <nav className="menu">
-      <ul>
+      <ul className="flex justify-start items-center md:items-start">
         {menuItems.map((item) => (
-          <li key={item.key}>
-            <NavLink to={item.route ?? "/"}>{item.label}</NavLink>
+          <li className="text-sm" key={item.key}>
+            <Button variant="ghost" className="flex gap-1">
+              <span>{item?.icon}</span>
+              <NavLink className="" to={item.route ?? "/"}>{item.label}</NavLink>
+            </Button>
           </li>
         ))}
       </ul>
