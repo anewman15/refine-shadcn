59cc6729a3a0e06a672afe80c8f493b6d9123c4f
Abdullah Numan <anewman15@hotmail.com>
2024-03-12 11:48:51 +0600

    feat: rework CategoryEdit with Shadcn comps

--- src/pages/categories/edit.tsx
+++ src/pages/categories/edit.tsx
@@ -1,56 +1,71 @@
+import React from "react";
 import { IResourceComponentsProps, useNavigation } from "@refinedev/core";
 import { useForm } from "@refinedev/react-hook-form";
-import React from "react";
+
+import { Form, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+
+import { ChevronLeft, ListIcon } from "lucide-react";
 
 export const CategoryEdit: React.FC<IResourceComponentsProps> = () => {
   const { list } = useNavigation();
 
   const {
     refineCore: { onFinish },
-    register,
-    handleSubmit,
-    formState: { errors },
+    ...form
   } = useForm({});
 
+  const { formState: {errors}, handleSubmit, register, setValue } = form;
+
   return (
-    <div style={{ padding: "16px" }}>
-      <div style={{ display: "flex", justifyContent: "space-between" }}>
-        <h1>Edit</h1>
+    <div className="p-2">
+      <div className="flex justify-between">
+        <div className="flex justify-between items-center gap-2">
+          <Button
+            variant="ghost"
+            size="sm"
+            onClick={() => list("categories")}
+          >
+            <ChevronLeft />
+          </Button>
+          <h1 className="text-2xl font-bold">Edit Category</h1>
+        </div>
         <div>
-          <button
+          <Button
+            variant="outline"
+            size="sm"
             onClick={() => {
               list("categories");
             }}
           >
-            List
-          </button>
+            <ListIcon />
+          </Button>
         </div>
       </div>
-      <form onSubmit={handleSubmit(onFinish)}>
-        <div
-          style={{
-            display: "flex",
-            flexDirection: "column",
-            gap: "8px",
-          }}
-        >
-          <label>
-            <span style={{ marginRight: "8px" }}>Title</span>
-            <input
-              type="text"
-              {...register("title", {
-                required: "This field is required",
-              })}
-            />
-            <span style={{ color: "red" }}>
-              {(errors as any)?.title?.message as string}
-            </span>
-          </label>
-          <div>
-            <input type="submit" value="Save" />
+      <Form {...form}>
+        <form onSubmit={handleSubmit(onFinish)} className="mt-6">
+          <div
+            className="flex flex-col gap-2"
+          >
+            <FormItem>
+              <FormLabel className="mr-2">Title</FormLabel>
+              <Input
+                type="text"
+                {...register("title", {
+                  required: "Title cannot be empty",
+                })}
+              />
+              <FormMessage className="text-destructive">
+                {(errors as any)?.title?.message as string}
+              </FormMessage>
+            </FormItem>
+            <div className="mt-4">
+              <Button className="w-40" type="submit">Save</Button>
+            </div>
           </div>
-        </div>
-      </form>
+        </form>
+      </Form>
     </div>
   );
 };
